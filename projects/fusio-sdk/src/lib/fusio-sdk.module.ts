659397d43ceef7736e51de7ccdc69454fcbe39cb
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-10-16 20:22:58 +0200

    update code style

--- projects/fusio-sdk/src/lib/fusio-sdk.module.ts
+++ projects/fusio-sdk/src/lib/fusio-sdk.module.ts
@@ -31,11 +31,11 @@ import {DetailComponent as EventDetail} from './component/event/detail/detail.co
 import {ListComponent as EventList} from './component/event/list/list.component';
 import {ModalComponent as EventModal} from './component/event/modal/modal.component';
 import {AccountContainerComponent} from "./component/account-container/account-container.component";
-import { HomeComponent } from './component/home/home.component';
-import { FaqComponent } from './component/faq/faq.component';
-import { NavigationComponent } from './component/navigation/navigation.component';
-import { PricingComponent } from './component/pricing/pricing.component';
-import { BackendContainerComponent } from './component/backend-container/backend-container.component';
+import {HomeComponent} from './component/home/home.component';
+import {FaqComponent} from './component/faq/faq.component';
+import {NavigationComponent} from './component/navigation/navigation.component';
+import {PricingComponent} from './component/pricing/pricing.component';
+import {BackendContainerComponent} from './component/backend-container/backend-container.component';
 
 @NgModule({
   declarations: [
