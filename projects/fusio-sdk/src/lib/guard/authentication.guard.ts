2470c3a75bb3945ca8b881262bc98d7bf06203ad
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-10-16 20:38:46 +0200

    update routes

--- projects/fusio-sdk/src/lib/guard/authentication.guard.ts
+++ projects/fusio-sdk/src/lib/guard/authentication.guard.ts
@@ -1,20 +1,34 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateChild,
+  Router,
+  RouterStateSnapshot,
+  UrlTree
+} from '@angular/router';
 import {Observable} from 'rxjs';
 import {FusioService} from "../service/fusio.service";
+import {ConsumerService} from "../service/consumer.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthenticationGuard implements CanActivate {
+export class AuthenticationGuard implements CanActivate, CanActivateChild {
 
-  constructor(private fusio: FusioService<any>, private router: Router) {
+  constructor(private consumer: ConsumerService, private router: Router) {
   }
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (this.fusio.hasValidToken()) {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.consumer.hasValidToken()) {
+      return true;
+    } else {
+      return this.router.createUrlTree(['login']);
+    }
+  }
+
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.consumer.hasValidToken()) {
       return true;
     } else {
       return this.router.createUrlTree(['login']);
--- projects/fusio-sdk/src/lib/util/route-builder.ts
+++ projects/fusio-sdk/src/lib/util/route-builder.ts
@@ -14,28 +14,42 @@ import {ConfirmComponent} from "../component/password/confirm/confirm.component"
 import {CallbackComponent} from "../component/subscription/callback/callback.component";
 import {ListComponent as AppListComponent} from '../component/app/list/list.component';
 import {ListComponent as EventListComponent} from '../component/event/list/list.component';
+import {HomeComponent} from "../component/home/home.component";
+import {FaqComponent} from "../component/faq/faq.component";
+import {PricingComponent} from "../component/pricing/pricing.component";
+import {BackendContainerComponent} from "../component/backend-container/backend-container.component";
+import {ImprintComponent} from "../component/imprint/imprint.component";
 
 export class RouteBuilder {
 
-  public static getRoutes(): Routes {
+  public static getRoutes(children: Routes): Routes {
     return [
-      {path: 'account', component: AccountContainerComponent, canActivate: [AuthenticationGuard], children: [
-        {path: '', component: AccountComponent, canActivate: [AuthenticationGuard]},
-        {path: 'security', component: SecurityComponent, canActivate: [AuthenticationGuard]},
-        {path: 'app', component: AppListComponent, canActivate: [AuthenticationGuard]},
-        {path: 'app/:id', component: AppListComponent, canActivate: [AuthenticationGuard]},
-        {path: 'event', component: EventListComponent, canActivate: [AuthenticationGuard]},
-        {path: 'event/:id', component: EventListComponent, canActivate: [AuthenticationGuard]},
-        {path: 'subscription', component: SubscriptionComponent, canActivate: [AuthenticationGuard]},
-        {path: 'subscription/callback/:plan_id', component: CallbackComponent, canActivate: [AuthenticationGuard]},
-      ]},
+      {path: '', component: HomeComponent},
+      {path: 'faq', component: FaqComponent},
+      {path: 'pricing', component: PricingComponent},
+      {path: 'imprint', component: ImprintComponent},
       {path: 'login', component: LoginComponent},
       {path: 'login/:provider', component: ProviderComponent},
       {path: 'logout', component: LogoutComponent},
       {path: 'register', component: RegisterComponent},
       {path: 'register/activate/:token', component: ActivateComponent},
       {path: 'password/reset', component: ResetComponent},
-      {path: 'password/confirm/:token', component: ConfirmComponent}
+      {path: 'password/confirm/:token', component: ConfirmComponent},
+      {path: 'account', component: AccountContainerComponent, canActivate: [AuthenticationGuard], children: this.getAccountRoutes()},
+      {path: 'backend', component: BackendContainerComponent, canActivate: [AuthenticationGuard], children: children},
+    ];
+  }
+
+  private static getAccountRoutes(): Routes {
+    return [
+      {path: '', component: AccountComponent},
+      {path: 'security', component: SecurityComponent},
+      {path: 'app', component: AppListComponent},
+      {path: 'app/:id', component: AppListComponent},
+      {path: 'event', component: EventListComponent},
+      {path: 'event/:id', component: EventListComponent},
+      {path: 'subscription', component: SubscriptionComponent},
+      {path: 'subscription/callback/:plan_id', component: CallbackComponent},
     ];
   }
 
