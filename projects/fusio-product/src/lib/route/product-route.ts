6f714322782314c9be71bc9d06b37d37ee2e0896
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-07-09 10:35:18 +0200

    use authorization route

--- projects/fusio-product/src/lib/route/product-route.ts
+++ projects/fusio-product/src/lib/route/product-route.ts
@@ -3,32 +3,21 @@ import {HomeComponent} from "../component/home/home.component";
 import {FaqComponent} from "../component/faq/faq.component";
 import {PricingComponent} from "../component/pricing/pricing.component";
 import {ImprintComponent} from "../component/imprint/imprint.component";
-import {
-  ActivateComponent,
-  ConfirmComponent,
-  LoginComponent,
-  LogoutComponent,
-  ProviderComponent,
-  RegisterComponent,
-  ResetComponent
-} from "ngx-fusio-sdk";
+import {AuthorizationRoute} from "ngx-fusio-sdk";
 
 export class ProductRoute {
 
   public static getAll(): Routes {
-    return [
+    const routes = [
       {path: '', component: HomeComponent},
-      {path: 'faq', component: FaqComponent},
-      {path: 'pricing', component: PricingComponent},
-      {path: 'imprint', component: ImprintComponent},
-      {path: 'login', component: LoginComponent},
-      {path: 'login/:provider', component: ProviderComponent},
-      {path: 'logout', component: LogoutComponent},
-      {path: 'register', component: RegisterComponent},
-      {path: 'register/activate/:token', component: ActivateComponent},
-      {path: 'password/reset', component: ResetComponent},
-      {path: 'password/confirm/:token', component: ConfirmComponent},
+      {path: 'faq', component: FaqComponent, title: 'FAQ'},
+      {path: 'pricing', component: PricingComponent, title: 'Pricing'},
+      {path: 'imprint', component: ImprintComponent, title: 'Imprint'},
     ];
+
+    routes.push(...AuthorizationRoute.getAll());
+
+    return routes;
   }
 
 }
