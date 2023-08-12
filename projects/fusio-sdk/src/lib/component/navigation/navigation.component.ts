5843e549c7764026b037288ca24908535752aab9
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-08-12 21:52:25 +0200

    add navigation service and component

---
+++ projects/fusio-sdk/src/lib/component/navigation/navigation.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {GroupItem, NavigationService} from "../../service/navigation.service";
+import {ConfigService} from "../../service/config.service";
+
+@Component({
+  selector: 'fusio-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent implements OnInit {
+
+  title?: string;
+  version?: string;
+  items: Array<GroupItem> = [];
+
+  constructor(private navigation: NavigationService, private config: ConfigService) {
+  }
+
+  ngOnInit(): void {
+    this.title = this.config.getTitle() || 'Fusio';
+    this.version = this.config.getVersion();
+    this.items = this.navigation.getMainNavigation();
+  }
+
+  changeNavHeading(item: GroupItem): void {
+    for (let i = 0; i < this.items.length; i++) {
+      this.items[i].visible = this.items[i].title === item.title
+    }
+  }
+
+}
---
+++ projects/fusio-sdk/src/lib/service/navigation.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import {BackendService} from "ngx-fusio-sdk";
+import {ConfigService} from "./config.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NavigationService {
+
+  constructor(private backend: BackendService, private config: ConfigService) {
+  }
+
+  getMainNavigation(): Array<GroupItem> {
+    return this.checkPermissions(this.config.getNavigation());
+  }
+
+  getUserNavigation(): Array<Item> {
+    return this.checkPermissionsItems(this.config.getUserNavigation());
+  }
+
+  getAccountNavigation(): Array<Item> {
+    return this.checkPermissionsItems(this.config.getAccountNavigation());
+  }
+
+  private checkPermissions(navigation: Array<GroupItem>): Array<GroupItem> {
+    let result = [];
+    for (let i = 0; i < navigation.length; i++) {
+      const children = this.checkPermissionsItems(navigation[i].children);
+      if (children.length > 0) {
+        let menu = navigation[i];
+        menu.children = children;
+        result.push(menu);
+      }
+    }
+
+    return result;
+  }
+
+  private checkPermissionsItems(items: Array<Item>): Array<Item> {
+    let result = [];
+    for (let i = 0; i < items.length; i++) {
+      const scope = items[i].scope;
+      if (scope && !this.backend.hasScope(scope)) {
+        continue;
+      }
+
+      result.push(items[i])
+    }
+
+    return result;
+  }
+
+}
+
+export interface GroupItem {
+  title: string
+  visible: boolean
+  children: Array<Item>
+}
+
+export interface Item {
+  title: string
+  icon?: string
+  path: string
+  scope?: string
+}
