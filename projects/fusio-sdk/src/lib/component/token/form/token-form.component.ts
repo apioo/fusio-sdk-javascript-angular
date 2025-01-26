82c52457869a1f945c841bb4386cc2bfc34fda91
Christoph Kappestein <christoph.kappestein@gmail.com>
2025-01-26 19:59:10 +0100

    update

--- projects/fusio-sdk/src/lib/component/token/form/token-form.component.ts
+++ projects/fusio-sdk/src/lib/component/token/form/token-form.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
-import {ConsumerToken} from "fusio-sdk";
+import {ConsumerToken, ConsumerTokenAccessToken} from "fusio-sdk";
 import {ErrorService} from "../../../service/error.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {Form} from "../../../abstract/form";
 import {TokenService} from "../../../service/token.service";
 import {ScopeService} from "../../../service/scope.service";
+import {error} from "ng-packagr/lib/utils/log";
 
 @Component({
   selector: 'fusio-token-form',
@@ -13,12 +14,18 @@ import {ScopeService} from "../../../service/scope.service";
 })
 export class TokenFormComponent extends Form<ConsumerToken> {
 
+  accessToken?: ConsumerTokenAccessToken;
+
   constructor(private token: TokenService, public scope: ScopeService, route: ActivatedRoute, router: Router, error: ErrorService) {
     super(route, router, error);
   }
 
   protected getService(): TokenService {
     return this.token;
+  }
+
+  protected override onSubmit() {
+    this.accessToken = this.token.getToken();
   }
 
 }
--- projects/fusio-sdk/src/lib/component/webhook/form/webhook-form.component.ts
+++ projects/fusio-sdk/src/lib/component/webhook/form/webhook-form.component.ts
@@ -4,6 +4,7 @@ import {ErrorService} from "../../../service/error.service";
 import {Form} from "../../../abstract/form";
 import {ActivatedRoute, Router} from "@angular/router";
 import {WebhookService} from "../../../service/webhook.service";
+import {EventService} from "../../../service/event.service";
 
 @Component({
   selector: 'fusio-webhook-form',
@@ -12,7 +13,7 @@ import {WebhookService} from "../../../service/webhook.service";
 })
 export class WebhookFormComponent extends Form<ConsumerWebhook> {
 
-  constructor(private webhook: WebhookService, route: ActivatedRoute, router: Router, error: ErrorService) {
+  constructor(private webhook: WebhookService, public event: EventService, route: ActivatedRoute, router: Router, error: ErrorService) {
     super(route, router, error);
   }
 
