3b7b10cbc17397bcd915ee72d03726d59ea2fa80
Christoph Kappestein <christoph.kappestein@gmail.com>
2025-11-24 22:13:03 +0100

    use signal

--- projects/fusio-sdk/src/lib/component/form/map/form-map.component.ts
+++ projects/fusio-sdk/src/lib/component/form/map/form-map.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, effect, EventEmitter, input, Input, Output, signal} from '@angular/core';
 import {Service} from "../../../abstract/service";
 import {FormAutocompleteComponent} from "../autocomplete/form-autocomplete.component";
 import {FormsModule} from "@angular/forms";
@@ -12,61 +12,73 @@ import {FormsModule} from "@angular/forms";
   ],
   styleUrls: ['./form-map.component.css']
 })
-export class FormMapComponent implements OnInit {
+export class FormMapComponent {
 
-  @Input() name!: string;
-  @Input() disabled: boolean = false;
-  @Input() type: string = 'text';
-  @Input() data: Record<string, string> = {};
+  name = input.required<string>();
+  disabled = input<boolean>(false);
+  type = input<string>('text');
+  data = input<Record<string, string>>({});
+  useTilde = input<boolean>(false);
+  useId = input<boolean>(false);
+
   @Input() service?: Service<any>;
-  @Input() useTilde: boolean = false;
-  @Input() useId: boolean = false;
   @Output() dataChange = new EventEmitter<Record<string, string>>();
 
-  local: Array<Entry> = [];
-  newKey: string = '';
-  newValue: any = '';
+  local = signal<Array<Entry>>([]);
+  newKey = signal<string>('');
+  newValue = signal<any>('');
 
-  constructor() { }
-
-  ngOnInit(): void {
-    if (this.data) {
-      this.local = this.toLocal(this.data);
-    }
+  constructor() {
+    effect(async () => {
+      this.local.set(this.toLocal(this.data()));
+    });
   }
 
   doChange(index: number, value?: any) {
-    this.local[index].value = value;
+    this.local.update((entries) => {
+      entries[index].value = value;
+      return entries;
+    })
+
     this.dataChange.emit(this.fromLocal());
   }
 
   doAdd() {
-    if (!this.newKey || !this.newValue) {
+    let newKey = this.newKey();
+    let newValue = this.newValue();
+
+    if (!newKey || !newValue) {
       return;
     }
 
-    let newValue = this.newValue;
-    if (this.type === 'number') {
+    if (this.type() === 'number') {
       newValue = parseInt(newValue);
     }
 
-    this.local.push({
-      key: this.newKey,
-      value: newValue,
+    this.local.update((entries) => {
+      entries.push({
+        key: newKey,
+        value: newValue,
+      });
+      return entries;
     });
 
-    this.newKey = '';
-    this.newValue = '';
+    this.newKey.set('');
+    this.newValue.set('');
     this.dataChange.emit(this.fromLocal());
 
-    const elKey = document.getElementById(this.name + '-newKey');
+    const elKey = document.getElementById(this.name() + '-newKey');
     if (elKey instanceof HTMLInputElement) {
       elKey.focus();
     }
   }
 
   doRemove(index: number) {
-    this.local.splice(index, 1);
+    this.local.update((entries) => {
+      entries.splice(index, 1);
+      return entries;
+    });
+
     this.dataChange.emit(this.fromLocal());
   }
 
@@ -83,7 +95,7 @@ export class FormMapComponent implements OnInit {
 
   fromLocal(): Record<string, any> {
     let data: Record<string, any> = {};
-    this.local.forEach((entry: Entry) => {
+    this.local().forEach((entry: Entry) => {
       data[entry.key] = entry.value;
     });
     return data;
